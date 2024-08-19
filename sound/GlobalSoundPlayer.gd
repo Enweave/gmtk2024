aacd5e3171cf91095d2543eb0af4210ca418328d
Enweave <enweave@gmail.com>
2024-08-20 04:56:03 +0500

    export stuff

--- sound/GlobalSoundPlayer.gd
+++ sound/GlobalSoundPlayer.gd
@@ -11,19 +11,21 @@ func _ready():
 
 
 func play_bgm(bgm: GlobalPlaylists.bgms):
-	if not bgm in GlobalPlaylists.bgm_map:
-		push_warning("BGM " + str(bgm) + " not found")
-		return
-	if last_bgm == bgm:
-		return
-	sound_player.stop()
-	last_bgm = bgm
-	sound_player.stream = load(GlobalPlaylists.bgm_map[bgm])
-	if bgm == GlobalPlaylists.bgms.MAIN:
-		sound_player.volume_db = -6
-	else:
-		sound_player.volume_db = 0
-	sound_player.play()
+	pass
+	
+#	if not bgm in GlobalPlaylists.bgm_map:
+#		push_warning("BGM " + str(bgm) + " not found")
+#		return
+#	if last_bgm == bgm:
+#		return
+#	sound_player.stop()
+#	last_bgm = bgm
+#	sound_player.stream = load(GlobalPlaylists.bgm_map[bgm])
+#	if bgm == GlobalPlaylists.bgms.MAIN:
+#		sound_player.volume_db = -6
+#	else:
+#		sound_player.volume_db = 0
+#	sound_player.play()
 
 
 func stop_bgm():
