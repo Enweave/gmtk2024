42bd5c9a921f9a44a39770b1064a70d5fcefe53a
Enweave <enweave@gmail.com>
2024-09-04 04:04:18 +0500

    hidden wall

---
+++ actors/triggers/hidden_wall_sensor/hidden_wall_sensor.gd
@@ -0,0 +1,32 @@
+@tool
+extends Node2D
+
+class_name HiddenWallSensor
+var fade_duration: float = 0.5
+
+@export var tile_map: TileMap
+
+@export var width: int = 1280:
+	set(value):
+		width = value
+		update_bounds()
+
+@export var height: int = 720:
+	set(value):
+		height = value
+		update_bounds()
+
+func update_bounds():
+	%Bounds.shape['size'] = Vector2(width, height)
+	
+
+func _on_area_2d_body_entered(_body):
+	if _body is Player and tile_map != null:
+		print("HiddenWallSensor::_on_area_2d_body_entered")
+		var tween: Tween = get_tree().create_tween()
+		tween.tween_property(tile_map, 'modulate:a', 0, fade_duration)
+		
+func _on_area_2d_body_exited(_body):
+	if _body is Player and tile_map != null:
+		var tween: Tween = get_tree().create_tween()
+		tween.tween_property(tile_map, 'modulate:a', 1, fade_duration)
