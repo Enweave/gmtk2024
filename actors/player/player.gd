6fd59944f09eac7c1c2a2ceb03f46d51c0d9c24f
Erik Ho <eho61@my.bcit.ca>
2024-09-05 00:22:23 -0700

    Added secret area to level 3

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -174,10 +174,10 @@ func _physics_process(delta):
 	update_animations()
 
 	# push rigid bodies
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+	#for i in get_slide_collision_count():
+		#var c = get_slide_collision(i)
+		#if c.get_collider() is RigidBody2D:
+			#c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 
 
 func _process(_delta):
